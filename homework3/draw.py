30ccc86cd9ae2907ededab632a834c0dc7ff3b9c
Ashleigh Antal <ashleigh.antal@gmail.com>
2020-09-24 19:14:32 -0400

    TurtleHW

---
+++ homework3/draw.py
@@ -0,0 +1,24 @@
+import turtle
+
+pen = turtle.Pen()
+pen.speed(1)
+pen.width(5)
+
+pen.color("red")
+pen.setheading(135)
+pen.forward(100)
+pen.setheading(220)
+pen.forward(200)
+pen.setheading(315)
+pen.forward(100)
+
+pen.color("white")
+pen.goto(0,0)
+
+pen.color("blue")
+pen.setheading(45)
+pen.forward(100)
+pen.setheading(315)
+pen.forward(200)
+pen.setheading(220)
+pen.forward(100)
